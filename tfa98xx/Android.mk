d42127de2803ae1a98bc2e116e8fa317e5877289
Arian <arian.kulmer@web.de>
2020-11-29 01:36:36 +0100

    davinci: tfa98xx: Make it compile for now
    Change-Id: I911ce99728e8752adbbe5ce0e979025e71a1d52e

--- tfa98xx/Android.mk
+++ tfa98xx/Android.mk
@@ -26,12 +26,14 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_C_INCLUDES := \
     $(call include-path-for, audio-route) \
     $(call include-path-for, audio-utils) \
-    $(call project-path-for, qcom-audio)/hal/audio_extn \
-    $(call project-path-for, qcom-audio)/hal/msm8974 \
-    external/tinycompress/include
+    external/tinycompress/include \
+    kernel/xiaomi/sm6150/techpack/audio/include/uapi \
+    vendor/qcom/opensource/audio-hal/primary-hal/hal \
+    vendor/qcom/opensource/audio-hal/primary-hal/hal/msm8974 \
+    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn
 
 LOCAL_HEADER_LIBRARIES := \
-    generated_kernel_headers \
+    qti_kernel_headers \
     libhardware_headers
 
 LOCAL_SHARED_LIBRARIES := \
